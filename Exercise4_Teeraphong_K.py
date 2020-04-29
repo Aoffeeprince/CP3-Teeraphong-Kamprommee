cbc4d1061cf7197a8d6f36be31ddb5345be9ee01
Teeraphong.K <64195963+Aoffeeprince@users.noreply.github.com>
2020-04-29 11:33:30 +0700

    Exercise4_Teeraphong_K.py

---
+++ Exercise4_Teeraphong_K.py
@@ -0,0 +1,9 @@
+FE = 78.0
+GB = 82.5
+IC = 90.0
+CP = 89.75
+print("------Your Score------")
+print("Foundation English : คะแนนที่ได้ =", FE)
+print("General Business : คะแนนที่ได้ =", GB)
+print("Introduction to Computer Systems : คะแนนที่ได้ =", IC)
+print("Computer Programming : คะแนนที่ได้ =", CP)
